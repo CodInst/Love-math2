d64aea2a2c273d6ba6c39c75b08da450a29ea8be
CodInst <a344y7jr@students.codeinstitute.net>
2025-03-27 11:18:34 +0000

    Set up structure of game functions

---
+++ assets/Js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+    
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+function incrementScore() {
+    
+}
+
+function incementWrongAnswer() {
+    
+}
+
+function displayAdditionQuestion() {
+    
+}
+
+function displaySubstractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
+function displayDivideQuestion() {
+    
+}
